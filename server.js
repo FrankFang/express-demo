631771f026b7e4747e7395dad8072e2671135f34
frankfang <frankfang1990@gmail.com>
2017-05-30 23:15:02 +0800

    自动加载下一页

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (request, response) {
-  let page = request.query.page || 1
+  let page = 1
   let db = []
-  for(let i=0; i<1000; i++){
+  for(let i=0; i<30; i++){
     db.push({
       title: `标题${i+1}`,
       content: `正文${i+1}, 文字很多，随便表示一下`
@@ -21,6 +21,23 @@ app.get('/', function (request, response) {
     currentPage: page
   }
   response.render('home', data);
+});
+app.get('/paged', function (request, response) {
+  let page = parseInt(request.query.page || 1)
+  let db = []
+  for(let i=0; i<30; i++){
+    db.push({
+      title: `标题${i+1}`,
+      content: `正文${i+1}, 文字很多，随便表示一下`
+    })
+  }
+  let data = {
+    articles: db.slice(10*(page-1), 10*page),
+    totalPages: Math.ceil(db.length / 10),
+    currentPage: page
+  }
+  response.setHeader('Content-Type', 'application/json');
+  response.send(JSON.stringify(data));
 });
 
   app.listen(3000);
